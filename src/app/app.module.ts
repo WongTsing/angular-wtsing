924f8bfb0077a582f639120ba633aeec03122850
WongTsing <892789755@qq.com>
2019-09-19 14:58:48 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { MydeComponent } from './myde/myde.component';
 
 @NgModule({
   imports: [
